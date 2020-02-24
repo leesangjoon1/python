f6bdf5b8d4e59fab15e1c4601e90b87bd7659758
leesangjoon1 <50565808+leesangjoon1@users.noreply.github.com>
2020-02-24 16:28:38 +0900

    Add files via upload

---
+++ def best_stock(dictionary).py
@@ -0,0 +1,16 @@
+def best_stock(a):
+    # your code here
+    
+    b = {key: value for key, value in a.items() if value == max(a.values())}
+    x = list(b.keys())
+    return x[0]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert best_stock({"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}) == "ATX"
+    assert best_stock({"CAC": 91.1, "ATX": 1.01, "TASI": 120.9}) == "TASI"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
