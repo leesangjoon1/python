a03a8b4b8fdcba900ef858edbd709a2a289a2f63
leesangjoon1 <50565808+leesangjoon1@users.noreply.github.com>
2020-02-24 16:27:03 +0900

    Add files via upload

---
+++ def checkio number 123405(mult) = 120.py
@@ -0,0 +1,23 @@
+def checkio(number):
+    result = 1
+    for s in str(number):
+        n = int(s)
+        if n == int(0):
+            continue
+        
+        result *= n
+        
+            
+    return result
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(123405))
+    
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
